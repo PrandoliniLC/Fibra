15f6909e3672782e0da03d726c33385f2015d961
Mark Prandolini <mark.prandolini@desy.de>
2026-09-06 17:40:07 +0200

    jhjj

--- src/GNLSE.jl
+++ src/GNLSE.jl
@@ -194,6 +194,7 @@ function gain_saturated2(Pin0::Float64, gssdB::Float64, PsatdBm::Float64)
     gain = gss / (1.0 + Pin0/Psat)
     return gain
 end
+
 #=
 """
 Need to finish this function, it is not tested yet
